eb09effce7a2de6197edb0bc70d00ef0c459f038
Christian Richter <christian.richter@live.com>
2015-01-31 23:58:34 +0100

    Parameter handling with minimist

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
 var express = require('express');
 var path = require('path');
+var argv = require('minimist')(process.argv.slice(2));
 var leasesParser = require('./modules/leasesParser');
 
-var leaseFilePath = '';
+var leaseFilePath = '/var/lib/dhcp/dhcpd.lease';
 var inactive = false;
 var hostname = 'localhost';
 var port = '8080';
 
-process.argv.forEach(function(param, index, array) {
-  var inactiveRegEx = /(-i|--inactive)/;
-  var leaseFileRegEx = /(-il|-l|--leaseFile)/;
-  var hostNameRegEx = /(-h|--hostname)/;
-
-  if (leaseFileRegEx.test(param)) {
-    leaseFilePath = array[index + 1];
-  }
-
-  if (inactiveRegEx.test(param)) {
+if (argv.i || argv.inactive) {
+  if (argv.i === true || argv.inactive === true) {
     inactive = true;
+  } else {
+    console.error('Are you sure you want to show inactives?');
   }
+}
 
-  if (hostNameRegEx.test(param)) {
-    var splitted = array[index + 1].split(':');
-    hostname = splitted[0] || 'localhost';
-    port = splitted[1] || 8080;
+if (argv.l || argv.leaseFile) {
+  if ((argv.l && argv.l !== true) ||
+      (argv.leaseFile && argv.leaseFile !== true)) {
+    leaseFilePath = argv.l || argv.leaseFile;
+  } else {
+    console.error('Incorrect path to lease file');
   }
-});
+}
 
-
-if (!leaseFilePath) {
-  console.error('Must define lease file');
-  return;
+if (argv.h || argv.hostname) {
+  if ((argv.h && argv.h !== true) ||
+      (argv.hostname && argv.hostname !== true)) {
+    hostname = (argv.h || argv.hostname).split(':');
+    port = hostname[1] || 8080;
+    hostname = hostname[0];
+  } else {
+    console.error('Incorrect hostname');
+  }
 }
 
 var app = express();
